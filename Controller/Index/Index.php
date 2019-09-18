12f62b529fd55eab66e57fae4b5a32dfdbd7d6f7
Dmitry Fedyuk <dfediuk@gmail.com>
2019-09-18 13:26:12 +0600

    0.0.2

---
+++ Controller/Index/Index.php
@@ -0,0 +1,18 @@
+<?php
+namespace TFC\Report\Controller\Index;
+use Magento\Framework\App\Action\Action as _P;
+use Magento\Framework\Controller\ResultFactory as F;
+use Magento\Framework\View\Result\Page;
+// 2019-09-18
+class Index extends _P {
+	/**    
+	 * 2019-09-18
+	 * @override
+	 * @see _P::execute()    
+	 * @used-by \Magento\Framework\App\Action\Action::dispatch():
+	 * 		$result = $this->execute();
+	 * https://github.com/magento/magento2/blob/2.2.1/lib/internal/Magento/Framework/App/Action/Action.php#L84-L125
+	 * @return Page
+	 */
+	function execute() {return $this->resultFactory->create(F::TYPE_PAGE);}
+}
